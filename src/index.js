fedacf4619c97a2dc766ae673c78534c734e456e
nagashima.toru <nagashima.toru@tis.co.jp>
2018-03-08 17:40:02 +0900

    Redux のチュートリアルをとりあえず入れてみる。

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+ 
+let store = createStore(todoApp)
+ 
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
